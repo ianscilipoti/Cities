8d3c177e9683bb3dc173ec289efdd47ae75240c3
ianscilipoti <ianscilipoti@gmail.com>
2020-07-15 22:58:36 -0400

    testing

---
+++ Assets/Scripts/LinkedGraph/LinkedGraph.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedGraph<EdgeType> where EdgeType : LinkedGraphEdge
+{
+    public static EdgeType AddEdge(LinkedGraphVertex aVert, LinkedGraphVertex bVert, ILinkedGraphEdgeFactory<EdgeType> edgeFactory, List<EdgeType> knownEdges)
+    {
+        LinkedGraphEdge newEdge = edgeFactory.GetEdge(aVert, bVert);
+        newEdge.a.AddConnection(newEdge);
+        newEdge.b.AddConnection(newEdge);
+
+        knownEdges.Add(newEdge);
+
+        return newEdge;
+    }
+}
